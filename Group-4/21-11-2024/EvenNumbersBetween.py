0f29171a9abdeca10d64d5e9f42ed72b303e01a6
Zahrashani <zahrashani2006@gmail.com>
2024-11-21 22:55:09 +0530

    Update EvenNumbersBetween.py

--- Group-4/21-11-2024/EvenNumbersBetween.py
+++ Group-4/21-11-2024/EvenNumbersBetween.py
@@ -1,4 +1,6 @@
-#Write a program to display even numbers between two numbers given by the user 
-
-
-#Write your code here
+# Program to display even numbers between two numbers given by the user
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+for number in range(a,b):
+    if number%2==0:
+      print(number)
