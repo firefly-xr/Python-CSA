3d242c95bf678279d154a4ffd2c426277436835a
Sulthana353 <Sulthanathazneem@gmail.com>
2024-11-21 22:33:49 +0530

    Update and rename 1.py to Palindrome.py

---
+++ Group-1/Palindrome.py
@@ -0,0 +1,14 @@
+#To check whether a number is a palindrome or not
+
+num=int(input("Enter a number: "))
+temp=num
+rev=0
+while(temp>0):
+  r=temp%10
+  temp=temp//10
+  rev=rev*10+r
+print(f"Reverse of {num} is {rev}")
+if rev==num:
+   print(f"{num} is a palindrome")
+else:
+   print(f"{num} is not a palindrome")
