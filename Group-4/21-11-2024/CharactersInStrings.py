a374354482d64111592036bcbb11fbc1f33a8949
hamna4code <hamna6311@gmail.com>
2024-11-21 23:16:10 +0530

    Update CharactersInStrings.py

--- Group-4/21-11-2024/CharactersInStrings.py
+++ Group-4/21-11-2024/CharactersInStrings.py
@@ -1,4 +1,8 @@
 #Write a program to count the number of times a character occurs in a given string
-
-#Write your answer here
-
+string=input("Enter a string")
+char=input("Enter a character")
+count=0
+for i in range(len(string)):
+	if(string[i]==char):
+		count=count+1
+print("The total number of times",char,"has occured=",count)
