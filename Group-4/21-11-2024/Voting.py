6136d74ae6938b62d2d16a4ecf6a3ef104c6d221
shehinshabeer <shehinshabeer9@gmail.com>
2024-11-22 08:33:24 +0530

    Update Voting.py

--- Group-4/21-11-2024/Voting.py
+++ Group-4/21-11-2024/Voting.py
@@ -1,1 +1,6 @@
-#Write your code here
+a=int(input("Enter Your Age"))
+if a>=18:
+        print("You are eligible for Voting")
+else:
+        print("You are not eligible for Voting")
+print("Thank You")
