29fe09f2b2144d6fb89bc301659c1944bbd7b273
Ain Shamsudheen <85692201+firefly-xr@users.noreply.github.com>
2024-11-25 19:44:29 +0530

    Update starpyramid.py

--- Group-2/starpyramid.py
+++ Group-2/starpyramid.py
@@ -6,9 +6,8 @@ for i in range(1, rows+1):
     for space in range(1, (rows-i)+1):
         print(end="  ")
    
-    while k!=(2*i-1):
+    while x!=(2*i-1):
         print("* ", end="")
         x += 1
-   
-    x = 0
-    print()
+    x=0
+    print()
