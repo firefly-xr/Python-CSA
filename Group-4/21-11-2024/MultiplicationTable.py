7f6fa3a7d18e54254d766857e518eea7804bbad5
Ain Shamsudheen <85692201+firefly-xr@users.noreply.github.com>
2024-11-25 22:15:41 +0530

    Update MultiplicationTable.py

--- Group-4/21-11-2024/MultiplicationTable.py
+++ Group-4/21-11-2024/MultiplicationTable.py
@@ -1,4 +1,9 @@
 #Write a program to accept a number from the user and print the multiplication table of that number using while loop
+number=int(input("Enter the number for its multiplication table: "))
 
+print(f"Multiplication table of {number} up to 10th term:")
+
+for i in range(1, 11):
+      print(f"{number} x {i} ={number * i}")
 
 #Write your code here
