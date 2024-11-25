bcaadb1c2818ac1aba78063c26b3ebe8217ebd54
Zahwasharaf <zahwasharaf65@gmail.com>
2024-11-25 21:54:48 +0530

    Create Numbers.py

---
+++ Group-7/Numbers.py
@@ -0,0 +1,12 @@
+numbers=[3,7,11,33,41,71]
+first_two=numbers :2]
+print( "original list:" , numbers)
+print("first two numbers:", first_two)
+if len(numbers) >7:
+    print("the list has more than 7 elements.")
+else:
+    print("the list has 7 or fewer elements.")
+if 11 in numbers:
+    print("the number 11 is in the list .")
+numbers ［4］="63"
+print (numbers)
