cd4d846eb47f20afdc962afbe64c3850e4e94720
abhijith-ml <abhijithml64@gmail.com>
2024-11-26 12:33:56 +0530

    Update find-prime-numbr.py

--- Group-2/find-prime-numbr.py
+++ Group-2/find-prime-numbr.py
@@ -1,15 +1,17 @@
-def is_prime(number):
-    if number <= 1:
-        return False  # Numbers less than or equal to 1 are not prime
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return False  # Number is divisible by a number other than 1 and itself
-    return True
+num = int(input("Enter the number: "))
 
-# Input from the user
-num = int(input("Enter a number: "))
-
-if is_prime(num):
-    print(f"{num} is a prime number.")
+if num <= 1:
+    print('Number is not a prime number.')
 else:
-    print(f"{num} is not a prime number.")
+    prime_value = True 
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            prime_value = False 
+            break 
+    
+    if prime_value:
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
+
+
