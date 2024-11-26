52a40d6629f8af8151c608a4f23f73efe1c12a6a
Malavika2142006 <malavikamurukesan996@gmail.com>
2024-11-26 12:30:34 +0530

    Create tuple.py

---
+++ Group-7/tuple.py
@@ -0,0 +1,7 @@
+#acessing elements of tuple
+colors=("red","green","blue","yellow")
+print("first color:",colors[0])
+print("Last color:",colors[-1])
+#slicing  the tuple
+middle_colors=colors[1:3]
+print("middle colors:",middle_colors)
