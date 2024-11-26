d945e0e444745321f5a86f3fc807760594c8459d
Yashna2005 <yashnazrin@gmail.com>
2024-11-26 12:20:10 +0530

    Create List.py

---
+++ Group-X/List.py
@@ -0,0 +1,11 @@
+# Example of a list storing project file names
+project_files = []
+
+# Adding files to the list
+project_files.append("main.py")
+project_files.append("utils.py")
+project_files.append("README.md")
+
+# Displaying the files in the list
+print("\nFiles in the project:")
+print(project_files)
