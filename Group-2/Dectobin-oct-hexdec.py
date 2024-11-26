65bc0f2c45ddef62b57fb4446f79024f536564f5
abhijith-ml <abhijithml64@gmail.com>
2024-11-26 12:11:54 +0530

    Update Dectobin-oct-hexdec.py

--- Group-2/Dectobin-oct-hexdec.py
+++ Group-2/Dectobin-oct-hexdec.py
@@ -1,4 +1,4 @@
-#to convert dec to binary, octal and hexadecimal
+##to convert dec to binary, octal and hexadecimal
 n=int(input("Enter the number: ")) 
 print("The dec value of", x, "is:")
 print(bin(n), "in binary.")
