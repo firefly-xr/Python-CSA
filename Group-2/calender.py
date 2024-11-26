46e3e10238ea9225dfffcbb60f301b62c7a7540f
Dhyanrajeshjohn <dhyanrjohn@gmail.com>
2024-11-26 12:20:31 +0530

    Update calender.py

--- Group-2/calender.py
+++ Group-2/calender.py
@@ -1,4 +1,4 @@
 import calendar
-yy=int(input("Enter the year:"))
-mm=int(input("Enter the month:"))#0 to 12
-print(calendar.month(yy,mm))
+yr=int(input("Enter the year:"))
+mn=int(input("Enter the month:"))#0 to 12
+print(calendar.month(yr,mn))
