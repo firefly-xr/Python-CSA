eefefed2cf1eb5537f7ed9f1e2dfa923d2b9346b
Muhammed Thoufeek S <thoufeeqpunalur@gmail.com>
2024-11-26 12:50:58 +0600

    Update BMI-Calculator.py

--- Group-2/BMI-Calculator.py
+++ Group-2/BMI-Calculator.py
@@ -1,17 +1,13 @@
-# Calculates BMI and provides a category
-def calculate_bmi(weight, height):
-    bmi = weight / (height ** 2)
-    if bmi < 18.5:
-        category = "Underweight"
-    elif 18.5 <= bmi < 24.9:
-        category = "Normal weight"
-    elif 25 <= bmi < 29.9:
-        category = "Overweight"
-    else:
-        category = "Obesity"
-    return bmi, category
+bmi = weight / (height ** 2)
+if bmi < 18.5:
+	print("Underweight")
+elif 18.5 <= bmi < 24.9:
+	print("Normal weight")
+elif 25 <= bmi < 29.9:
+	print("Overweight")
+else:
+	print("Enter Currect Digit")
 
-weight = float(input("Enter weight in kg: "))
-height = float(input("Enter height in meters: "))
-bmi, category = calculate_bmi(weight, height)
-print(f"Your BMI is {bmi:.2f} ({category}).")
+
+weight = input("Enter weight in kg: ")
+height = input("Enter height in meters: ")
