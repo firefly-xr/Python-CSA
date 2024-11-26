ded151465d8722e545d9c165f964d8178dd6236a
Muhammedasif123 <muhammedasif13577@gmail.com>
2024-11-26 08:50:53 +0530

    Update SumOfEven&Odd.py

--- Group-4/21-11-2024/SumOfEven&Odd.py
+++ Group-4/21-11-2024/SumOfEven&Odd.py
@@ -1,1 +1,16 @@
+#Write a program to display the sum of even numbers and odd number s up to n entered by the user
+ 
 #Write your code here
+
+n=int(input("Enter a number:"))
+o=0
+e=0
+for i in range (n):
+        if i%2==0:
+                e=e+i
+        else:
+                o=o+i
+print("even=",e)
+print("odd=",o)
+
+
