f763eef42e00e7513983521ac5d9377494eb0d35
Hamdinajeeb <hamdiprvte0@gmail.com>
2024-11-26 12:07:43 +0530

    Create Month name to a number of days.py

---
+++ Group-9/Month name to a number of dyas.py
@@ -0,0 +1,13 @@
+# given list of month name
+month = ["January", "April", "August","June","Dovember"]
+ 
+# iterate through each mont in the list
+for i in month:
+    if i == "February":
+            print("The month of February has 28/29 days")
+    elif i in ("April", "June", "September", "November"):
+            print("The month of",i,"has 30 days.")
+    elif i in ("January", "March", "May", "July", "August", "October", "December"):
+            print("The month of",i,"has 31 days.")
+    else:
+            print(i,"is not a valid month name.")
