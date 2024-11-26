143b64b6f2e5d53a8087109059b2f84eb645bc95
abhijith-ml <abhijithml64@gmail.com>
2024-11-26 12:06:53 +0530

    Update Dectobin-oct-hexdec.py

--- Group-2/Dectobin-oct-hexdec.py
+++ Group-2/Dectobin-oct-hexdec.py
@@ -1,6 +1,6 @@
-#to convert decimal to binary, octal and hexadecimal
+#to convert dec to binary, octal and hexadecimal
 x=int(input("Enter the number: ")) 
-print("The decimal value of", x, "is:")
+print("The dec value of", x, "is:")
 print(bin(x), "in binary.")
 print(oct(x), "in octal.")
 print(hex(x), "in hexadecimal.")
