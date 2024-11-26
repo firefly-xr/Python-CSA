25dbc77a63587263498a31133c4659b6efe21de1
Gouthamplayz <94751939+Gouthamplayz@users.noreply.github.com>
2024-11-26 12:19:12 +0530

    Update Celsius-to-Fahrenheit.py

--- Group-2/Celsius-to-Fahrenheit.py
+++ Group-2/Celsius-to-Fahrenheit.py
@@ -1,4 +1,4 @@
 # Converts Celsius to Fahrenheit
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(f"Temperature in Fahrenheit: {fahrenheit}")
+cel = float(input("Enter temperature in Celsius: "))
+fah = (cel * 9/5) + 32
+print(f"Temperature in Fahrenheit: {fah}")
