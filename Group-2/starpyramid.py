56f7d33b379b79477d3c4f31376280bdd2fc54fa
Gouthamplayz <94751939+Gouthamplayz@users.noreply.github.com>
2024-11-26 12:12:53 +0530

    Update starpyramid.py

--- Group-2/starpyramid.py
+++ Group-2/starpyramid.py
@@ -1,13 +1,13 @@
 rows = int(input("Enter number of rows: "))
 
-x = 0
+y = 0
 
 for i in range(1, rows+1):
     for space in range(1, (rows-i)+1):
         print(end="  ")
    
-    while x!=(2*i-1):
+    while y!=(2*i-1):
         print("* ", end="")
-        x += 1
-    x=0
+        y += 1
+    y=0
     print()
